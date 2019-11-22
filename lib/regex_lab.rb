dc5e85c945716078b53c97a0d13af72240a88b24
Alfredo Diaz <mail4alfredo@gmail.com>
2019-11-22 18:06:34 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,8 +1,8 @@
 require 'pry'
 
 def starts_with_a_vowel?(word)
- # word.match(/^[aeiouAEIOU]\w+/) ? true : false
- binding.pry
+ word.match(/^[aeiouAEIOU]\w+/) ? true : false
+ #binding.pry
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
